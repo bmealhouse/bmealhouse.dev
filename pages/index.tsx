7dda87ccb7975e0d70851725ecf5ef8de569b08f
Brent Mealhouse <bmealhouse@gmail.com>
2021-02-28 11:29:41 -0600

    Add favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,10 @@ export default function Home() {
     <div className="flex flex-col items-center justify-center min-h-screen py-28 text-gray-900 dark:bg-gray-900 dark:text-gray-50">
       <Head>
         <title>Brent Mealhouse</title>
-        {/* <link rel="icon" href="/favicon.ico" /> */}
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🦄</text></svg>"
+        />
         <meta name="description" content="" />
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
       </Head>
