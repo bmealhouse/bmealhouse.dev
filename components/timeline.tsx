3107785284c68f9fc13d9d6411b3a7f11585a7d3
Brent Mealhouse <bmealhouse@gmail.com>
2021-03-13 10:18:40 -0600

    Remove content to shorten resume

--- components/timeline.tsx
+++ components/timeline.tsx
@@ -9,7 +9,7 @@ interface Props {
 
 export const Timeline = ({ children }: Props) => (
   <div className="flow-root">
-    <h2 className="mt-12 text-2xl font-extrabold uppercase">Timeline</h2>
+    <h2 className="mt-12 text-2xl font-extrabold uppercase">Experience</h2>
     <ul className="mt-4 -mb-8">{children}</ul>
   </div>
 );
