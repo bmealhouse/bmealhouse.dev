10dc8c692ea18833797edd24c481b9d7f4a32d45
Brent Mealhouse <bmealhouse@gmail.com>
2021-09-25 08:45:34 -0500

    Add SAM to skills

--- pages/index.tsx
+++ pages/index.tsx
@@ -39,6 +39,7 @@ export default function Home() {
               "React Testing Library",
               "Node.js",
               "AWS",
+              "SAM",
               "Serverless",
               "DynamoDB",
               "ElastiCache",
